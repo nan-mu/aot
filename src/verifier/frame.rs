6587cfd32c2f835e0395c8d3ebac2a7f4d0721b8
nan mu <mu.nan.11@outlook.com>
2026-04-19 01:17:05 +0800

    ✨ 放弃使用verifier；使用c2rust工具替代ai翻译

--- src/verifier/frame.rs
+++
@@ -1,9 +0,0 @@
-// Extracted from /Users/nan/bs/aot/src/verifier.c
-static u32 frame_insn_idx(struct bpf_verifier_state *st, u32 frame)
-{
-	return frame == st->curframe
-	       ? st->insn_idx
-	       : st->frame[frame + 1]->callsite;
-}
-
-
